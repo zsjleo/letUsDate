65b1299e9f2d504bc05b73943e788c8654ec9d88
zsjleo <zsjleo@live.cn>
2017-10-25 14:01:06 +0800

    Initial Commit

---
+++ letUsChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  letUsChat
+//
+//  Created by Herer on 2017/10/25.
+//  Copyright © 2017年 Herer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ letUsChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  letUsChat
+//
+//  Created by Herer on 2017/10/25.
+//  Copyright © 2017年 Herer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
